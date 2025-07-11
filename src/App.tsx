1e641fc80594fd9c4f5d7e5a3a71449fdaecae5e
paquerettee <paquerettee@gmail.com>
2025-07-11 16:13:31 +0200

    add PageNotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Post } from "./pages/Post";
+import { PageNotFound } from "./pages/PageNotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/post/:id" element={<Post />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const PageNotFound: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen text-center">
+      <h1 className="text-5xl font-bold text-red-600 mb-4">404 - Page Not Found</h1>
+      <p className="text-gray-600 my-4">Oops! That page doesn’t exist.</p>
+      <a href="/" className="text-blue-600 hover:underline">
+        ← Go back home
+      </a>
+    </div>
+  );
+};
